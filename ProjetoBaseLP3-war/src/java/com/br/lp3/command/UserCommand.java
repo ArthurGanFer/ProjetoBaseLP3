896d925a6663585d974d491419948144740d49a1
31338283 <31338283@087877-P33S105.macklabs.br>
2016-02-18 09:17:16 -0200

    Commit Inicial

---
+++ ProjetoBaseLP3-war/src/java/com/br/lp3/command/UserCommand.java
@@ -0,0 +1,46 @@
+package com.br.lp3.command;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author 1147106
+ */
+public class UserCommand implements Command{
+    public HttpServletRequest request;
+    public HttpServletResponse response;
+    public String returnPage = "index.jsp";
+   
+    @Override
+    public void init(HttpServletRequest request, HttpServletResponse response) {
+        this.request = request;
+        this.response = response;
+    }
+
+    @Override
+    public void execute() {
+        String action = request.getParameter("action");
+        switch(action){
+            case "login":
+                String username = request.getParameter("username");
+                String password = request.getParameter("password");
+                
+                if("cacique".equals(username) && "123".equals(password)){
+                    returnPage = "index.jsp";
+                } else {
+                    returnPage = "login.jsp";
+                }
+                
+                break;
+        }
+    }
+
+    @Override
+    public String getReturnPage() {
+        return returnPage;
+    }
+    
+    
+    
+}
